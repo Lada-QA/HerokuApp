3f1013dd5cb88478815d0930ae3fd9b2ca72097a
Ladauser <sovyhka29@gmail.com>
2021-10-21 16:55:59 +0300

    add file test Typos

---
+++ src/test/java/tests/HerokuAppTyposTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.List;
+
+public class HerokuAppTyposTest {
+
+    @Test
+    public void herokuAppTyposTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/typos");
+        List<WebElement> text = driver.findElements(By.cssSelector("#content > div > p:nth-child(3)"));
+        for (WebElement checkText : text) {
+            Assert.assertEquals(checkText.getText(),
+                    "Sometimes you'll see a typo, other times you won't.");
+        }
+    }
+}
