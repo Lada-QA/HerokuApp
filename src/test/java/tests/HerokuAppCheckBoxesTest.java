86544d5771270907456c276731cc8af8119ab7a8
Ladauser <sovyhka29@gmail.com>
2021-10-21 22:53:29 +0300

    Fixed file test CheckBoxes

--- src/test/java/tests/HerokuAppCheckBoxesTest.java
+++ src/test/java/tests/HerokuAppCheckBoxesTest.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-    public class HerokuAppCheckBoxesTest {
+public class HerokuAppCheckBoxesTest {
 
     @Test
-    public void herokuAppCheckBoxesTest() {
+    public void herokuAppCheckBox1Test() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--incognito");
@@ -18,29 +19,27 @@ import org.testng.annotations.Test;
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/checkboxes");
-        WebElement checked = driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)"));
-        WebElement checked2 = driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)"));
-        if (checked.isSelected()) {
-            System.out.println("Check");
-        } else
-            System.out.println("UnCheck");
-        driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)"));
-        driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)")).click();
-        if (checked.isSelected()) {
-            System.out.println("Check");
-        } else
-            System.out.println("UnCheck");
-        driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)"));
-        if (checked2.isSelected()) {
-            System.out.println("Check");
-        } else
-            System.out.println("UnCheck");
-        driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)")).click();
-        if (checked2.isSelected()) {
-            System.out.println("Check");
-        } else
-            System.out.println("UnCheck");
+        WebElement checkbox1 = driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)"));
+        Assert.assertFalse(checkbox1.isSelected());
+        checkbox1.click();
+        Assert.assertTrue(checkbox1.isSelected());
+        driver.quit();
+    }
 
+    @Test
+    public void herokuAppCheckBox2Test() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        WebElement checkbox2 = driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)"));
+        driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)"));
+        Assert.assertTrue(checkbox2.isSelected());
+        checkbox2.click();
+        Assert.assertFalse(checkbox2.isSelected());
         driver.quit();
     }
 }
