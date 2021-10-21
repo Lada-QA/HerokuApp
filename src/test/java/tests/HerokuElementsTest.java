a325aa5e50c518f81f3e80719952ad1981d20294
Ladauser <sovyhka29@gmail.com>
2021-10-21 22:34:40 +0300

    Fixed file test ElementsTest

--- src/test/java/tests/HerokuElementsTest.java
+++ src/test/java/tests/HerokuElementsTest.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HerokuElementsTest {
 
     @Test
-    public void herokuAddRemoveElementsTest (){
+    public void herokuAddRemoveElementsTest() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--incognito");
@@ -18,13 +19,12 @@ public class HerokuElementsTest {
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
-        WebElement addRemoveElements = driver.findElement(By.xpath("//*[@onclick='addElement()']"));
-        addRemoveElements.findElement(By.xpath("//*[@onclick='addElement()']")).click();
-        addRemoveElements.findElement(By.xpath("//*[@onclick='addElement()']")).click();
-        addRemoveElements.findElement(By.xpath("//*[@onclick='deleteElement()']")).click();
-        int count = addRemoveElements.findElements(By.xpath("//*[@onclick='deleteElement()']")).size();
-        System.out.println(count);
-
+        WebElement addElement = driver.findElement(By.xpath("//*[@onclick='addElement()']"));
+        addElement.click();
+        addElement.click();
+        addElement.findElement(By.xpath("//*[@onclick='deleteElement()']")).click();
+        int count = addElement.findElements(By.xpath("//*[@onclick='deleteElement()']")).size();
+        Assert.assertEquals(count, 1);
         driver.quit();
     }
 }
